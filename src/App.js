93541f8613d42a810a1a4347a660650776177b75
Stephen Ferrari <stephen.ferrari14@gmail.com>
2020-03-16 00:35:54 -0400

    Got auth working with sign up and sign in

--- src/App.js
+++ src/App.js
@@ -1,26 +1,135 @@
 import React from 'react';
-import logo from './logo.svg';
+import Container from '@material-ui/core/Container';
+import MuscleApp from './components/MuscleApp';
 import './App.css';
+import firebase from './components/Firebase/firebase'
+import { format, startOfWeek } from 'date-fns'
+import SignIn from './components/auth/SignIn';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect
+} from "react-router-dom";
+import SignUp from './components/auth/SignUp';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    calendarRef: null,
+    muscleGroupsRef: null,
+    muscleData: [],
+    muscleGroups: [],
+    hasSignedIn: false
+  }
+
+  onAddSession = (newMuscle) => {
+    const newWorkoutSession = this.state.calendarRef.push()
+    newWorkoutSession.set({
+      muscleGroupId: newMuscle,
+      date: format(new Date(), 'yyyy-MM-dd')
+    })
+  }
+
+  signOut = () => {
+    this.setState({ hasSignedIn: false })
+  }
+
+  // Move to service file
+  createNewUser = (username, password) => {
+    return firebase.auth().createUserWithEmailAndPassword(username, password)
+  }
+
+  sendResetPassword = (emailAddress) => auth.sendPasswordResetEmail(emailAddress)
+
+  // Guess I have to move it down so React Router works. If its below then I can use props.history
+  authenticateUser = (username, password) => {
+    return firebase.auth().signInWithEmailAndPassword(username, password).then((session) => {
+      this.setState({ hasSignedIn: true });
+      var calendarRef = firebase.database().ref(`/calendar-data/${session.user.uid}/${format(startOfWeek(new Date()), 'yyyy-MM-dd')}`);
+      const muscleGroupsRef = firebase.database().ref('/muscle-groups');
+      this.setState({ muscleGroupsRef }, () => {
+        this.state.muscleGroupsRef.on('value', (snapshot) => {
+          const muscleGroups = snapshot.val();
+          this.setState({ muscleGroups })
+        });
+      });
+
+      this.setState({ calendarRef }, () => {
+        this.state.calendarRef.on('value', (snapshot) => {
+          // This means that we can just push and update the app state from here instead of updated state and push also
+          const muscleData = snapshot.val() || {};
+          const formatMuscleData = Object.keys(muscleData).map((key) => {
+            return muscleData[key]
+          });
+          this.setState({ muscleData: formatMuscleData })
+        });
+      });
+    }).catch(err => console.log(err)) // Post a message
+  }
+
+  render() {
+    const PrivateRoute = ({ children, ...rest }) => {
+      return (
+        <Route
+          {...rest}
+          render={({ location }) =>
+            this.state.hasSignedIn ? (
+              children
+            ) : (
+                <Redirect
+                  to={{
+                    pathname: "/login",
+                    state: { from: location }
+                  }}
+                />
+              )
+          }
+        />
+      );
+    }
+    return (
+      <div className="App">
+        <Router forceRefresh={false}>
+          <Switch>
+            <PrivateRoute path="/calendar">
+              <Container maxWidth="sm">
+                <MuscleApp
+                  handleAddSession={this.onAddSession}
+                  muscleData={this.state.muscleData}
+                  muscleGroups={this.state.muscleGroups}
+                  handleSignOut={this.signOut}
+                />
+              </Container>
+            </PrivateRoute>
+            <Route path="/login">
+              <SignIn
+                authenticateUser={this.authenticateUser}
+              ></SignIn>
+            </Route>
+            <Route path="/signup">
+              <SignUp
+                createNewUser={this.createNewUser}
+              ></SignUp>
+            </Route>
+          </Switch>
+        </Router>
+        {/* {this.state.hasSignedIn ? (
+        <Container maxWidth="sm">
+          <MuscleApp 
+            handleAddSession={this.onAddSession}
+            muscleData={this.state.muscleData}
+            muscleGroups={this.state.muscleGroups}
+          />
+        </Container>
+        ) : (
+          <SignIn
+            authenticateUser={this.authenticateUser}
+          ></SignIn>
+        )} */}
+      </div>
+    );
+  }
 }
 
 export default App;
